ca6d85e501676ca890e1c5d440aaffd634724966
Shakil <shakil2995@gmail.com>
2022-03-04 23:10:28 +0600

    mongoose

--- mongoDb/app.js
+++ mongoDb/app.js
@@ -1,24 +1,36 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://localhost:27017/fruitsDB");
-
-const fruitSchema = new mongoose.Schema({
-    name :String,
-    rating:Number,
-    review:String
+const User = require('./user');
+mongoose.connect("mongodb://localhost:27017/fruitsDB",()=>{
+    console.log("Db Connected");
 });
-const Fruit = mongoose.model("Fruit",fruitSchema);
+run();
+async function run(){
+    const user = new User({name:"orange",rating:5})
+    await user.save()
+    console.log(user);
+}
 
-const fruit = new Fruit({
-    name:"Apple",
-    rating:7,
-    review:"Pretty good"
-    },
-    {
-        name:"Orange",
-        rating:8,
-        review:"Pretty good too"
-    }
-)
+
+// const fruitSchema = new mongoose.Schema({
+//     name :String,
+//     rating:Number,
+//     review:String
+// });
+// const Fruit = mongoose.model("Fruit",fruitSchema);
+
+// const fruit = new Fruit({
+
+//     name:"Apple",
+//     rating:7,
+//     review:"Pretty good"
+//     },
+//     {
+//         name:"Orange",
+//         rating:8,
+//         review:"Pretty good too"
+//     }
+// );
+// fruit.save();
 
 
 
